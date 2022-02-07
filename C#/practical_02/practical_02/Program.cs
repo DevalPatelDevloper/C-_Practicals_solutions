e109f5f0f55dd008e8285542b75452d9f433596a
SF-CPU-311\deval <deval.p@simformsolutions.com>
2022-02-07 18:29:01 +0530

    C# practical commit

---
+++ C#/practical_02/practical_02/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practical_02
+{
+    class Customer_Account
+    {
+        //create 3 variable
+        public string bank_name;
+        private long customer_accountNo;
+        private string customer_name;
+        //create parameterized constructor 
+        public Customer_Account(long customer_accountNo, string customer_name)
+        {
+            this.customer_accountNo = customer_accountNo;
+            this.customer_name = customer_name;
+        }
+        /// <summary>
+        /// printinfo method prints bank name ,customer accountNo and customer name
+        /// </summary>
+        /// <returns></returns>
+        public string printinfo()
+        {
+                return bank_name + " " +customer_accountNo + " " + customer_name;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer_Account c1 = new Customer_Account(101, "Deval");//create object of class Customer_Account & Pass parameters
+            c1.bank_name = "BOB";//initialize bank_name
+            Console.WriteLine(c1.printinfo());//using refernce variable call printinfo method
+            Console.ReadLine();//display on console
+        }
+    }
+}
---
+++ C#/practical_05/practical_05/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practical_05
+{
+    class Program
+    {
+        /// <summary>
+        /// if we can arr[6] pass then error throw and Arr.length + 1 the error generate
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            int[] Arr={12,11,10,11,2 };
+            try
+            {
+                //Console.Write(Arr[6]);//error
+                for (int i = 0; i < Arr.Length; i++)//Arr.Length+1 then error 
+                {
+                    Console.Write(Arr[i] + " ");
+                }
+            }catch(Exception ex)
+            {
+                Console.Write(ex);
+            }
+            finally//always run finally block
+            {
+                Console.Write("\nfinally block");
+            }
+            Console.ReadLine();
+        }
+    }
+}
